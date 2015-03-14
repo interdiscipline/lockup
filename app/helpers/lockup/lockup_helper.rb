0c8b816e30ee8c562752a7743de10abdaa52b146
Grant Blakeman <hello@grantblakeman.com>
2015-03-14 12:34:10 -0600

    oops, shouldn't have namespaced Figaro specifically, no need

--- app/helpers/lockup/lockup_helper.rb
+++ app/helpers/lockup/lockup_helper.rb
@@ -2,7 +2,7 @@ module Lockup
   module LockupHelper
 
     def lockup_hint_present?
-      if ENV["LOCKUP_HINT"].present? || ENV["lockup_hint"].present? || Figaro.env.lockup_hint.present? || ((Rails::VERSION::MAJOR >= 4 && Rails::VERSION::MINOR >= 1) && Rails.application.secrets.lockup_hint.present?)
+      if ENV["LOCKUP_HINT"].present? || ENV["lockup_hint"].present? || ((Rails::VERSION::MAJOR >= 4 && Rails::VERSION::MINOR >= 1) && Rails.application.secrets.lockup_hint.present?)
         true
       else
         false
@@ -14,8 +14,6 @@ module Lockup
         ENV["LOCKUP_HINT"].to_s
       elsif ENV["lockup_hint"].present?
         ENV["lockup_hint"].to_s
-      elsif Figaro.env.lockup_hint.present?
-        Figaro.env.lockup_hint.to_s
       elsif (Rails::VERSION::MAJOR >= 4 && Rails::VERSION::MINOR >= 1) && Rails.application.secrets.lockup_hint.present?
         Rails.application.secrets.lockup_hint.to_s
       end
