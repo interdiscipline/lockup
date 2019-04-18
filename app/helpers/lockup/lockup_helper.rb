4f1fea9179b366e343b7610834946b519dca4146
Lawrence Matacena <1447611+guaguasi@users.noreply.github.com>
2019-04-18 19:42:34 -0400

    Simplified Lockup::LockupHelper

--- app/helpers/lockup/lockup_helper.rb
+++ app/helpers/lockup/lockup_helper.rb
@@ -1,23 +1,10 @@
 module Lockup
   module LockupHelper
-
-    def lockup_hint_present?
-      ENV["LOCKUP_HINT"].present? ||
-      ENV["lockup_hint"].present? ||
-      (Rails.application.respond_to?(:secrets) && Rails.application.secrets.lockup_hint.present?) ||
-      (Rails.application.respond_to?(:credentials) && Rails.application.credentials.lockup_hint.present?)
-    end
-
-    def lockup_hint_display
-      if ENV["LOCKUP_HINT"].present?
-        ENV["LOCKUP_HINT"].to_s
-      elsif ENV["lockup_hint"].present?
-        ENV["lockup_hint"].to_s
-      elsif Rails.application.respond_to?(:secrets) && Rails.application.secrets.lockup_hint.present?
-        Rails.application.secrets.lockup_hint.to_s
-      elsif Rails.application.respond_to?(:credentials) && Rails.application.credentials.lockup_hint.present?
-        Rails.application.credentials.lockup_hint.to_s
-      end
+    def lockup_hint
+      ENV['LOCKUP_HINT'] ||
+        ENV['lockup_hint'] ||
+        lockup_hint_from_config(:secrets) ||
+        lockup_hint_from_config(:credentials)
     end
 
     private
