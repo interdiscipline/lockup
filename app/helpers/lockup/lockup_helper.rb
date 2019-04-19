326c1dbc4ec3f23ba8b100e8332adbddd70c8658
Lawrence Matacena <1447611+guaguasi@users.noreply.github.com>
2019-04-18 20:24:46 -0400

    further simplified Lockup::LockupHelper

--- app/helpers/lockup/lockup_helper.rb
+++ app/helpers/lockup/lockup_helper.rb
@@ -6,20 +6,8 @@ module Lockup
       @lockup_hint ||=
         ENV['LOCKUP_HINT'] ||
         ENV['lockup_hint'] ||
-        lockup_hint_from_config(:secrets) ||
-        lockup_hint_from_config
-    end
-
-    private
-
-    def lockup_hint_from_config(secrets_or_credentials = :credentials)
-      return unless Rails.application.respond_to?(secrets_or_credentials)
-
-      store = Rails.application.public_send(secrets_or_credentials)
-
-      store.lockup.respond_to?(:fetch) &&
-        store.lockup.fetch(:hint, store.lockup_hint) ||
-        store.lockup_hint
+        ::Lockup.from_config(:hint, :secrets) ||
+        ::Lockup.from_config(:hint)
     end
   end
 end
